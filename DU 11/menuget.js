705356df2ab0c97e1f6a82640b1f472f8803b5a2
skibar101 <honzaskibar@gmail.com>
2023-12-15 18:46:23 +0100

    Edit

--- DU 11/menuget.js
+++ DU 11/menuget.js
@@ -8,15 +8,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
         links[3]='<li><a role="menuitem" class="nav-link  " href="https://moodle.spsejecna.cz/course/request.php" tabindex="-1">Požádat o kurz</a></li>';
         links[4]='<li><a role="menuitem" class="nav-link  " href="https://oldmoodle.spsejecna.cz/" tabindex="-1">Starý Moodle</a></li>';
         links[5]='<li><a href="#">Další</a></li>';
-           
-       
-    menu.innerHTML='';
-    
     for(i=0;i<Math.random() * 10;i++)
        {
        menu.innerHTML+=links[i];
        }
        
-    menu.innerHTML+='';
+  
 
 }); //DOMContentLoaded   
